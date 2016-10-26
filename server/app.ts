4ec129450b45916f5c626ee2fddcb530ee27d9d7
Swapp <Swapnil.Sawant@imail.org>
2016-10-25 19:39:12 -0600

    app works express basic

---
+++ server/app.ts
@@ -0,0 +1,50 @@
+import * as express from "express";
+import { json, urlencoded } from "body-parser";
+import { join } from "path";
+import * as cors from "cors";
+import * as compression from "compression";
+
+import { loginRouter } from "./routes/login";
+import { protectedRouter } from "./routes/protected";
+import { publicRouter } from "./routes/public";
+
+const app: express.Application = express();
+app.disable("x-powered-by");
+
+app.use(json());
+app.use(compression());
+app.use(urlencoded({ extended: true }));
+
+// allow cors only for local dev
+app.use(cors({
+  origin: "http://localhost:4200"
+}));
+
+// api routes
+app.use("/api", protectedRouter);
+app.use("/login", loginRouter);
+app.use("/public", publicRouter);
+
+if (app.get("env") === "production") {
+
+  // in production mode run application from dist folder
+  app.use(express.static(join(__dirname, '/../client')));
+}
+
+// catch 404 and forward to error handler
+app.use(function(req: express.Request, res: express.Response, next) {
+  let err = new Error("Not Found");
+  next(err);
+});
+
+// production error handler
+// no stacktrace leaked to user
+app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.status(err.status || 500);
+  res.json({
+    error: {},
+    message: err.message
+  });
+});
+
+export { app }
